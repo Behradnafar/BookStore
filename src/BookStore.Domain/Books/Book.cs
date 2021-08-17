7a75330848b84a7671c08b7c18c9b9157576c0db
Behradnafar <behrdnafar@gmail.com>
2021-08-17 15:08:33 -0400

    Add project files.

---
+++ src/BookStore.Domain/Books/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace BookStore.Books
+{
+    public class Book : AuditedAggregateRoot<Guid>
+    {
+        public virtual string Name { get; set; }
+
+        public virtual BookType Type { get; set; }
+
+        public virtual DateTime PublishDate { get; set; }
+
+        public virtual float Price { get; set; }
+    }
+}
